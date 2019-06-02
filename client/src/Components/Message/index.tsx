2df75aeea0bd4a6b176093e1d73dfcf90e14cbb0
Loïc <mimisteus@gmail.com>
2019-06-02 21:00:51 +0200

    Fixes to the message component

--- client/src/Components/Message/index.tsx
+++ client/src/Components/Message/index.tsx
@@ -10,11 +10,13 @@ const Message = ({
   const [messageVisibility, setMessageVisibility] = useState(false)
 
   useEffect(() => {
-    setTimeout(() => {
-      setMessageVisibility(false)
-    }, 5000)
+    if (message !== '') {
+      setTimeout(() => {
+        setMessageVisibility(false)
+      }, 5000)
 
-    setMessageVisibility(true)
+      setMessageVisibility(true)
+    }
   }, [message])
 
   return (
